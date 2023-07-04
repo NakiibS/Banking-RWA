792a67f95e37a573dd5e0849437c00d5981ebdcd
anuevo <adrian.nuevo@solera.com>
2023-07-04 17:00:21 +0200

    First commit in front

---
+++ frontend/real world app/rwa/src/App.jsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css";
+import Signup from "./frames/Signup/Signup";
+
+function App() {
+  return (
+    <>
+      <div className={styles.App}>
+
+        <Signup/>
+
+      </div>
+    </>
+  );
+}
+
+export default App;
